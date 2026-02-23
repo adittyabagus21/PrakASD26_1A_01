8f76de0dfa335c471287163a0cc34a2562536cb1
“adittyabagus21” <pratamatya211@gmail.com>
2026-02-23 19:15:14 +0700

    P2_Tugas2_01

---
+++ P2_01/Dosen01.java
@@ -0,0 +1,41 @@
+package P2_01;
+
+public class Dosen01 {
+    String idDosen;
+    String nama;
+    boolean statusAktif;
+    int tahunBergabung;
+    String bidangKeahlian;
+
+    public Dosen01 () {
+    }
+
+    public Dosen01 (String idDosen, String nama, boolean statusAktif, int tahunBergabung, String bidangKeahlian) {
+        this.idDosen = idDosen;
+        this.nama = nama;
+        this.statusAktif = statusAktif;
+        this.tahunBergabung = tahunBergabung;
+        this.bidangKeahlian = bidangKeahlian;
+    }
+
+    void tampilInformasi() {
+        System.out.println("idDosen        : " + idDosen);
+        System.out.println("nama           : " + nama);
+        System.out.println("statusAktif    : " + statusAktif);
+        System.out.println("tahunBergabung : " + tahunBergabung);
+        System.out.println("bidangKeahlian : " + bidangKeahlian);
+    }
+
+    void setStatusAktif(boolean status) {
+        statusAktif = status;
+    }
+
+    int hitungMasaKerja(int thnSkrg) {
+        return thnSkrg - tahunBergabung;
+    }
+
+    void ubahKeahlian(String bidang) {
+        bidangKeahlian = bidang;
+    }
+
+}
---
+++ P2_01/DosenMain01.java
@@ -0,0 +1,31 @@
+package P2_01;
+
+public class DosenMain01 {
+    public static void main(String[] args) {
+
+        Dosen01 d1 = new Dosen01 ();
+        d1.idDosen = "D001";
+        d1.nama = "Andi";
+        d1.statusAktif = true;
+        d1.tahunBergabung = 2018;
+        d1.bidangKeahlian = "Algoritma";
+
+        Dosen01 d2 = new Dosen01 ("D002", "Budi", false, 2015, "Basis Data");
+        
+        System.out.println("Data Dosen 1");
+        d1.tampilInformasi();
+        System.out.println("masaKerja      : " + d1.hitungMasaKerja(2026));
+        System.out.println();
+
+        System.out.println("Data Dosen 2");
+        d2.tampilInformasi();
+        System.out.println("masaKerja      : " + d2.hitungMasaKerja(2026));
+        System.out.println();
+
+        d2.setStatusAktif(true);
+        d2.ubahKeahlian("Pemrograman");
+
+        System.out.println("Data Dosen 2 Setelah Perubahan");
+        d2.tampilInformasi();
+    }
+}
