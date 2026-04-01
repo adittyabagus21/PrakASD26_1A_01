da27a60bbc3d1427bce7007ba43faa06f19527fe
“adittyabagus21” <pratamatya211@gmail.com>
2026-04-01 09:55:20 +0700

    P6_Percobaan2_1_01

---
+++ src/P6_01/Mahasiswa01.java
@@ -0,0 +1,23 @@
+public class Mahasiswa01 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa01() {
+    }
+
+    Mahasiswa01(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+}
