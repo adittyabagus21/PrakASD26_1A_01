c076bfebe8da68523aac91e8dafe3c3c7305c44e
adittyabagus21 <pratamatya211@gmail.com>
2026-05-12 11:29:03 +0700

    P11_Percobaan1_01

---
+++ src/P11_01/Percobaan1/SLLMain1.java
@@ -0,0 +1,21 @@
+package P11.Percobaan1;
+
+public class SLLMain1 {
+    public static void main(String[] args) {
+        SingleLinkedList1 sll = new SingleLinkedList1();
+
+        Mahasiswa1 mhs1 = new Mahasiswa1("21212203", "Dirga", "4D", 3.6);
+        Mahasiswa1 mhs2 = new Mahasiswa1("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa1 mhs3 = new Mahasiswa1("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa1 mhs4 = new Mahasiswa1("24212200", "Alvaro", "1A", 4.0); 
+
+        sll.print(); 
+        sll.addFirst(mhs1); 
+        sll.print();
+        sll.addLast(mhs4); 
+        sll.print();
+        sll.insertAfter("Dirga", mhs3); 
+        sll.insertAt(1, mhs2); 
+        sll.print();
+    }
+}
