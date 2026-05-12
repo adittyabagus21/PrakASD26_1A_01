db3b3b55b02d7f1533187587a05aa73f6447671d
adittyabagus21 <pratamatya211@gmail.com>
2026-05-12 11:40:49 +0700

    P11_Percobaan2_SLLMain_01

--- src/P11_01/Percobaan1/SLLMain1.java
+++ src/P11_01/Percobaan1/SLLMain1.java
@@ -1,24 +1,13 @@
 package P11_01.Percobaan1;
 
-import java.util.Scanner;
-
 public class SLLMain1 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SingleLinkedList1 sll = new SingleLinkedList1();
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan NIM: "); String nim = sc.nextLine();
-        System.out.print("Masukkan Nama: "); String nama = sc.nextLine();
-        System.out.print("Masukkan Kelas: "); String kelas = sc.nextLine();
-        System.out.print("Masukkan IPK: "); double ipk = sc.nextDouble();
-
-        Mahasiswa1 mhsBaru = new Mahasiswa1(nim, nama, kelas, ipk);
-        sll.addLast(mhsBaru);
-
-        Mahasiswa1 mhs1 = new Mahasiswa1("111", "Dirga", "1A", 3.8);
-        Mahasiswa1 mhs2 = new Mahasiswa1("112", "Budi", "1B", 3.6);
-        Mahasiswa1 mhs3 = new Mahasiswa1("113", "Tono", "1A", 3.5);
-        Mahasiswa1 mhs4 = new Mahasiswa1("114", "Rudi", "1C", 3.9);
+        Mahasiswa1 mhs1 = new Mahasiswa1("21212203", "Dirga", "4D", 3.6);
+        Mahasiswa1 mhs2 = new Mahasiswa1("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa1 mhs3 = new Mahasiswa1("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa1 mhs4 = new Mahasiswa1("24212200", "Alvaro", "1A", 4.0); 
 
         sll.print(); 
         sll.addFirst(mhs1); 
@@ -26,7 +15,19 @@ public class SLLMain1 {
         sll.addLast(mhs4); 
         sll.print();
         sll.insertAfter("Dirga", mhs3); 
-        sll.insertAt(1, mhs2); 
+        sll.insertAt(1, mhs2);
+        sll.print();
+
+        System.out.println("data index 1: ");
+        sll.getData(1);
+        System.out.println("data mahasiswa an Bimon berada pada index: " + sll.indexOf("Bimon"));
+        System.out.println();
+        
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        
+        sll.removeAt(0);
         sll.print();
     }
     
