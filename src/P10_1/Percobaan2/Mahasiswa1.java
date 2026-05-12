b0ef95d408241a0cce3616e4b599a2bc608573ad
adittyabagus21 <pratamatya211@gmail.com>
2026-05-12 10:59:25 +0700

    P10_Percobaan2 & Tugas_01

---
+++ src/P10_1/Percobaan2/Mahasiswa1.java
@@ -0,0 +1,19 @@
+package P10_1.Percobaan2;
+
+public class Mahasiswa1 {
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+
+    public Mahasiswa1(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println(nim + " " + nama + " " + prodi + " " + kelas);
+    }
+}
